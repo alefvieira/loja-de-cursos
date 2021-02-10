bbb5cb456e5f67dea8ca8090b9014e338d9a281e
alefvieira <78700687+alefvieira@users.noreply.github.com>
2021-02-10 14:10:06 -0300

    Add files via upload
    quarto modificação: correção da tag <h6> => <h4> e acrescentado background

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ export default function Home() {
       <main className={styles.main}>
 
     
-        <h3 className={styles.title}>
+        <h1 className={styles.title}>
           Welcome to <a href="/">NowFormOnline</a>
-        </h3>
+        </h1>
 
         <div className={styles.grid}>
 
@@ -33,7 +33,7 @@ export default function Home() {
           > 
             <h2>Por até R$100,00</h2>
             <h3>Curso de Desenho Método Fanart 3.0 </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>O Curso de Desenho Método Fan Art 3.0 irá te ensinar a desenhar o seu personagem favorito de Anime/Mangá, mesmo que você tenha o famoso "dom" para desenhar.
             </p>
             
@@ -46,7 +46,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Curso Design Gráfico </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Curso online COMPLETO do iniciante ao profissional. Aprenda a criar artes profissionais e trabalhe para gráficas e agências.
             </p>
           </a>
@@ -57,7 +57,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Curso de Informática Empresarial </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Com 6 cursos completos em vídeo aulas, você poderá assistir quando e onde quiser pelo celular ou em qualquer outro dispositivo. Não é necessário ter computador para assistir as aulas.
             </p>
           </a>
@@ -66,7 +66,7 @@ export default function Home() {
             rel="noopener"
           href="https://go.hotmart.com/G39347111D" className={styles.card}>
             <h3>Curso Auxiliar Administrativo </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Auxiliar Administrativo - O curso nº 1 para quem quer entrar no mercado de trabalho!</p>
           </a>
 
@@ -76,7 +76,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Como fazer uma Monografia de sucesso </h3>
-            <h6>Formato: eBooks, Documentos</h6>
+            <h4>Formato: eBooks, Documentos</h4>
             <p>Se você está iniciando sua MONOGRAFIA e não sabe por onde começar, este e-book é pra você!</p>
           </a>
 
@@ -86,7 +86,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>A de Abelha </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>
             Mais de 700 atividades infantis, para crianças em período de alfabetização, entre 02 a 08 anos.
             </p>
@@ -98,7 +98,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Site para Negócios </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Aprenda criar Sites Profissionais voltados para Empreendedores que desejam posicionar sua marca na internet e construir autoridade no seu nicho de forma eficiente.</p>
           </a>
 
@@ -106,7 +106,7 @@ export default function Home() {
             rel="noopener"
           href="https://go.hotmart.com/R41416796C" className={styles.card}>
             <h3>Curso Completo Manutenção e Conserto de Celular Versão 4.0 </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Curso completo do nível básico até o mais avançado onde o aluno aprende passo-a-passo a manutenção, conserto e reparo em diversos modelos de aparelho de celular de diversos fabricantes.</p>
           </a>
           
@@ -116,7 +116,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Pacote Apostilas Bolos de Sucesso </h3>
-            <h6>Formato: eBooks, Documentos</h6>
+            <h4>Formato: eBooks, Documentos</h4>
             <p>São 11 Apostilas, totalizando mais de 400 receitas completas de massas, coberturas, recheios, brigadeiros gourmet, bolos, tortas e muito mais.
             </p>
           </a>
@@ -126,7 +126,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Homem de Respeito </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>O Melhor Curso de Sedução da Internet! Torne-se um Homem de Respeito e Aprenda Como Conquistar uma Mulher. Dicas Exclusivas para ser um homem que as mulheres querem, O Curso homem veio para Revolucionar.
             </p>
           </a>
@@ -136,7 +136,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Fechamento Inteligente </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>O Fechamento Inteligente é um sistema que permite ao apostador otimizar suas apostas de maneira bastante inteligente.
             </p>
           </a>
@@ -147,7 +147,7 @@ export default function Home() {
           >
             <h2>Por até R$100,00</h2>
             <h3>E-Book Instagram para Negócios </h3>
-            <h6>Formato: eBooks, Documentos</h6>
+            <h4>Formato: eBooks, Documentos</h4>
             <p>Ebook em PDF com dicas essenciais para quem deseja transformar o seu instagram em uma conta comercial, atraente e totalmente profissional, para conquistar mais seguidores, enjangamento e fortalecimento da marca da empresa de forma eficiente e estratégica.
             </p>
             
@@ -158,7 +158,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Curso Operador de Caixa em Apostila </h3>
-            <h6>Formato: eBooks, Documentos</h6>
+            <h4>Formato: eBooks, Documentos</h4>
             <p>Operador de caixa é uma das áreas que mais crescem e necessitam de pessoas. Faça nosso curso e receba um certificado reconhecido em todo o território nacional.
             </p>
           </a>
@@ -168,7 +168,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Curso Maquiagem na Web 1.0 </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>O Curso Maquiagem na Web é totalmente Online, disponível por 1 ano, 24 horas por dia. São mais de 35 vídeo aulas+ atualizações de novas aulas + certificado Você pode fazer de onde quiser e em qualquer horário.
             </p>
           </a>
@@ -180,7 +180,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3> </h3>
-            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>
             </p>
           </a> */}
