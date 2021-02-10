4cedd844923150fc14b8eb29695bb9dd2ebdc063
alefvieira <78700687+alefvieira@users.noreply.github.com>
2021-02-10 18:42:22 -0300

    Add files via upload
    sexta atu: preços atualizados

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-
 export default function Home() {
-
+  
 
   return (
     <div className={styles.container}>
@@ -31,13 +30,11 @@ export default function Home() {
             href="https://go.hotmart.com/Y29172716T"
             className={styles.card}
           > 
-            <h2>Por até R$100,00</h2>
+            <h2>Por até R$97,00</h2>
             <h3>Curso de Desenho Método Fanart 3.0 </h3>
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>O Curso de Desenho Método Fan Art 3.0 irá te ensinar a desenhar o seu personagem favorito de Anime/Mangá, mesmo que você tenha o famoso "dom" para desenhar.
-            </p>
-            
-            
+            </p>         
           </a>
 
           <a target="_blank"
@@ -45,7 +42,7 @@ export default function Home() {
             href="https://go.hotmart.com/R39347453G"
             className={styles.card}
           >
-            <h2>Por até R$100,00</h2>
+            <h2>Por até R$197,00</h2>
             <h3>Curso Design Gráfico </h3>
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Curso online COMPLETO do iniciante ao profissional. Aprenda a criar artes profissionais e trabalhe para gráficas e agências.
@@ -57,7 +54,7 @@ export default function Home() {
             href="https://go.hotmart.com/T39347454X"
             className={styles.card}
           >
-            <h2>Por até R$100,00</h2>
+            <h2>Por até R$67,00</h2>
             <h3>Curso de Informática Empresarial </h3>
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Com 6 cursos completos em vídeo aulas, você poderá assistir quando e onde quiser pelo celular ou em qualquer outro dispositivo. Não é necessário ter computador para assistir as aulas.
@@ -67,7 +64,7 @@ export default function Home() {
           <a target="_blank"
             rel="noopener"
           href="https://go.hotmart.com/G39347111D" className={styles.card}>
-            <h2>Por até R$100,00</h2>
+            <h2>Por até R$97,00</h2>
             <h3>Curso Auxiliar Administrativo </h3>
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Auxiliar Administrativo - O curso nº 1 para quem quer entrar no mercado de trabalho!</p>
@@ -78,7 +75,7 @@ export default function Home() {
             href="https://go.hotmart.com/R39651724C"
             className={styles.card}
           >
-            <h2>Por até R$100,00</h2>
+            <h2>Por R$39,90</h2>
             <h3>Como fazer uma Monografia de sucesso </h3>
             <h4>Formato: eBooks, Documentos</h4>
             <p>Se você está iniciando sua MONOGRAFIA e não sabe por onde começar, este e-book é pra você!</p>
@@ -89,7 +86,7 @@ export default function Home() {
             href="https://go.hotmart.com/L47386117Q"
             className={styles.card}
           >
-            <h2>Por até R$100,00</h2>
+            <h2>Por R$34,90</h2>
             <h3>A de Abelha </h3>
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>
@@ -102,7 +99,7 @@ export default function Home() {
             href="https://go.hotmart.com/Q39355872J"
             className={styles.card}
           >
-            <h2>Por até R$100,00</h2>
+            <h2>Por R$247,00</h2>
             <h3>Site para Negócios </h3>
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Aprenda criar Sites Profissionais voltados para Empreendedores que desejam posicionar sua marca na internet e construir autoridade no seu nicho de forma eficiente.</p>
@@ -111,13 +108,13 @@ export default function Home() {
           <a target="_blank"
             rel="noopener"
           href="https://go.hotmart.com/R41416796C" className={styles.card}>
-            <h2>Por até R$100,00</h2>
+            <h2>Por até R$497,00</h2>
             <h3>Curso Completo Manutenção e Conserto de Celular Versão 4.0 </h3>
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>Curso completo do nível básico até o mais avançado onde o aluno aprende passo-a-passo a manutenção, conserto e reparo em diversos modelos de aparelho de celular de diversos fabricantes.</p>
           </a>
           
-          <a target="_blank"
+          {/* <a target="_blank"
             rel="noopener"
             href="https://go.hotmart.com/G29201386F"
             className={styles.card}
@@ -138,13 +135,14 @@ export default function Home() {
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>O Melhor Curso de Sedução da Internet! Torne-se um Homem de Respeito e Aprenda Como Conquistar uma Mulher. Dicas Exclusivas para ser um homem que as mulheres querem, O Curso homem veio para Revolucionar.
             </p>
-          </a>
+          </a> */}
+
           <a target="_blank"
             rel="noopener"
             href="https://go.hotmart.com/D29196891G"
             className={styles.card}
           >
-            <h2>Por até R$100,00</h2>
+            <h2>Por até R$97,00</h2>
             <h3>Fechamento Inteligente </h3>
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>O Fechamento Inteligente é um sistema que permite ao apostador otimizar suas apostas de maneira bastante inteligente.
@@ -155,7 +153,7 @@ export default function Home() {
             href="https://go.hotmart.com/V39355871I"
             className={styles.card}
           >
-            <h2>Por até R$100,00</h2>
+            <h2>Por R$19,90</h2>
             <h3>E-Book Instagram para Negócios </h3>
             <h4>Formato: eBooks, Documentos</h4>
             <p>Ebook em PDF com dicas essenciais para quem deseja transformar o seu instagram em uma conta comercial, atraente e totalmente profissional, para conquistar mais seguidores, enjangamento e fortalecimento da marca da empresa de forma eficiente e estratégica.
@@ -167,7 +165,7 @@ export default function Home() {
             href="https://go.hotmart.com/G39347449T"
             className={styles.card}
           >
-            <h2>Por até R$100,00</h2>
+            <h2>Por R$49,90</h2>
             <h3>Curso Operador de Caixa em Apostila </h3>
             <h4>Formato: eBooks, Documentos</h4>
             <p>Operador de caixa é uma das áreas que mais crescem e necessitam de pessoas. Faça nosso curso e receba um certificado reconhecido em todo o território nacional.
@@ -178,7 +176,7 @@ export default function Home() {
             href="https://go.hotmart.com/K39651548V"
             className={styles.card}
           >
-            <h2>Por até R$100,00</h2>
+            <h2>Por R$39,90</h2>
             <h3>Curso Maquiagem na Web 1.0 </h3>
             <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
             <p>O Curso Maquiagem na Web é totalmente Online, disponível por 1 ano, 24 horas por dia. São mais de 35 vídeo aulas+ atualizações de novas aulas + certificado Você pode fazer de onde quiser e em qualquer horário.
