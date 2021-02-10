d004ca380ac8a4891a058d8166bc1c56856e6612
alefvieira <78700687+alefvieira@users.noreply.github.com>
2021-02-10 17:14:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,213 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  
+
+  return (
+    <div className={styles.container}>
+      
+      <Head>
+      <title>Cursos online - aprenda o que quiser, quando quiser</title>
+        <link rel="icon" href="/logoQO.ico" />
+
+        <meta 
+        name="description"           
+        content="Cursos online, aprenda o que quiser, quando quiser. Transforme sua vida, se aperfeçoe e alcance seus objetivos e sonhos."></meta>
+      </Head>
+
+      <main className={styles.main}>
+
+    
+        <h1 className={styles.title}>
+          Welcome to <a href="/">NowFormOnline</a>
+        </h1>
+
+        <div className={styles.grid}>
+
+        <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/Y29172716T"
+            className={styles.card}
+          > 
+            <h2>Por até R$97,00</h2>
+            <h3>Curso de Desenho Método Fanart 3.0 </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>O Curso de Desenho Método Fan Art 3.0 irá te ensinar a desenhar o seu personagem favorito de Anime/Mangá, mesmo que você tenha o famoso "dom" para desenhar.
+            </p>         
+          </a>
+
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/R39347453G"
+            className={styles.card}
+          >
+            <h2>Por até R$197,00</h2>
+            <h3>Curso Design Gráfico </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>Curso online COMPLETO do iniciante ao profissional. Aprenda a criar artes profissionais e trabalhe para gráficas e agências.
+            </p>
+          </a>
+
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/T39347454X"
+            className={styles.card}
+          >
+            <h2>Por até R$67,00</h2>
+            <h3>Curso de Informática Empresarial </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>Com 6 cursos completos em vídeo aulas, você poderá assistir quando e onde quiser pelo celular ou em qualquer outro dispositivo. Não é necessário ter computador para assistir as aulas.
+            </p>
+          </a>
+
+          <a target="_blank"
+            rel="noopener"
+          href="https://go.hotmart.com/G39347111D" className={styles.card}>
+            <h2>Por até R$97,00</h2>
+            <h3>Curso Auxiliar Administrativo </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>Auxiliar Administrativo - O curso nº 1 para quem quer entrar no mercado de trabalho!</p>
+          </a>
+
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/R39651724C"
+            className={styles.card}
+          >
+            <h2>Por R$39,90</h2>
+            <h3>Como fazer uma Monografia de sucesso </h3>
+            <h4>Formato: eBooks, Documentos</h4>
+            <p>Se você está iniciando sua MONOGRAFIA e não sabe por onde começar, este e-book é pra você!</p>
+          </a>
+
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/L47386117Q"
+            className={styles.card}
+          >
+            <h2>Por R$34,90</h2>
+            <h3>A de Abelha </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>
+            Mais de 700 atividades infantis, para crianças em período de alfabetização, entre 02 a 08 anos.
+            </p>
+          </a>
+
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/Q39355872J"
+            className={styles.card}
+          >
+            <h2>Por R$247,00</h2>
+            <h3>Site para Negócios </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>Aprenda criar Sites Profissionais voltados para Empreendedores que desejam posicionar sua marca na internet e construir autoridade no seu nicho de forma eficiente.</p>
+          </a>
+
+          <a target="_blank"
+            rel="noopener"
+          href="https://go.hotmart.com/R41416796C" className={styles.card}>
+            <h2>Por até R$497,00</h2>
+            <h3>Curso Completo Manutenção e Conserto de Celular Versão 4.0 </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>Curso completo do nível básico até o mais avançado onde o aluno aprende passo-a-passo a manutenção, conserto e reparo em diversos modelos de aparelho de celular de diversos fabricantes.</p>
+          </a>
+          
+          {/* <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/G29201386F"
+            className={styles.card}
+          >
+            <h2>Por até R$100,00</h2>
+            <h3>Pacote Apostilas Bolos de Sucesso </h3>
+            <h4>Formato: eBooks, Documentos</h4>
+            <p>São 11 Apostilas, totalizando mais de 400 receitas completas de massas, coberturas, recheios, brigadeiros gourmet, bolos, tortas e muito mais.
+            </p>
+          </a>
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/V39651790O"
+            className={styles.card}
+          >
+            <h2>Por até R$100,00</h2>
+            <h3>Homem de Respeito </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>O Melhor Curso de Sedução da Internet! Torne-se um Homem de Respeito e Aprenda Como Conquistar uma Mulher. Dicas Exclusivas para ser um homem que as mulheres querem, O Curso homem veio para Revolucionar.
+            </p>
+          </a> */}
+
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/D29196891G"
+            className={styles.card}
+          >
+            <h2>Por até R$97,00</h2>
+            <h3>Fechamento Inteligente </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>O Fechamento Inteligente é um sistema que permite ao apostador otimizar suas apostas de maneira bastante inteligente.
+            </p>
+          </a>
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/V39355871I"
+            className={styles.card}
+          >
+            <h2>Por R$19,90</h2>
+            <h3>E-Book Instagram para Negócios </h3>
+            <h4>Formato: eBooks, Documentos</h4>
+            <p>Ebook em PDF com dicas essenciais para quem deseja transformar o seu instagram em uma conta comercial, atraente e totalmente profissional, para conquistar mais seguidores, enjangamento e fortalecimento da marca da empresa de forma eficiente e estratégica.
+            </p>
+            
+          </a>
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/G39347449T"
+            className={styles.card}
+          >
+            <h2>Por R$49,90</h2>
+            <h3>Curso Operador de Caixa em Apostila </h3>
+            <h4>Formato: eBooks, Documentos</h4>
+            <p>Operador de caixa é uma das áreas que mais crescem e necessitam de pessoas. Faça nosso curso e receba um certificado reconhecido em todo o território nacional.
+            </p>
+          </a>
+          <a target="_blank"
+            rel="noopener"
+            href="https://go.hotmart.com/K39651548V"
+            className={styles.card}
+          >
+            <h2>Por R$39,90</h2>
+            <h3>Curso Maquiagem na Web 1.0 </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>O Curso Maquiagem na Web é totalmente Online, disponível por 1 ano, 24 horas por dia. São mais de 35 vídeo aulas+ atualizações de novas aulas + certificado Você pode fazer de onde quiser e em qualquer horário.
+            </p>
+          </a>
+
+                    
+          {/* <a target="_blank"
+            rel="noopener"
+            href="#"
+            className={styles.card}
+          >
+            <h3> </h3>
+            <h4>Formato: Cursos Online, Serviços de Assinatura</h4>
+            <p>
+            </p>
+          </a> */}
+
+
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="/"
+          rel="noreferrer"
+        >
+          Powered by{' Álef Coutinho'}
+          <img src="/logoqualifiquese.png" width="150" height="150" alt="QualifiqueseOnline Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
