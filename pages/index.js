d495942575319c7e5c9d467a3210824013fe3851
alefvieira <78700687+alefvieira@users.noreply.github.com>
2021-02-10 13:42:46 -0300

    Add files via upload
    3 tualização correção de urls animação bloco de cursos e melhores colorações da fonte.

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,10 @@ export default function Home() {
       <Head>
       <title>Cursos online - aprenda o que quiser, quando quiser</title>
         <link rel="icon" href="/logoQO.ico" />
+
+        <meta 
+        name="description"           
+        content="Cursos online, aprenda o que quiser, quando quiser. Transforme sua vida, se aperfeçoe e alcance seus objetivos e sonhos."></meta>
       </Head>
 
       <main className={styles.main}>
@@ -23,145 +27,162 @@ export default function Home() {
         <div className={styles.grid}>
 
         <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/Y29172716T"
             className={styles.card}
-          >
-            <h3>Curso de Desenho Método Fanart 3.0 &rarr;</h3>
+          > 
+            <h2>Por até R$100,00</h2>
+            <h3>Curso de Desenho Método Fanart 3.0 </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>O Curso de Desenho Método Fan Art 3.0 irá te ensinar a desenhar o seu personagem favorito de Anime/Mangá, mesmo que você tenha o famoso "dom" para desenhar.
-            </h7>
+            <p>O Curso de Desenho Método Fan Art 3.0 irá te ensinar a desenhar o seu personagem favorito de Anime/Mangá, mesmo que você tenha o famoso "dom" para desenhar.
+            </p>
             
-            <p>Por até R$100,00</p>
+            
           </a>
 
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/R39347453G"
             className={styles.card}
           >
-            <h3>Curso Design Gráfico &rarr;</h3>
+            <h3>Curso Design Gráfico </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>Curso online COMPLETO do iniciante ao profissional. Aprenda a criar artes profissionais e trabalhe para gráficas e agências.
-            </h7>
+            <p>Curso online COMPLETO do iniciante ao profissional. Aprenda a criar artes profissionais e trabalhe para gráficas e agências.
+            </p>
           </a>
 
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/T39347454X"
             className={styles.card}
           >
-            <h3>Curso de Informática Empresarial &rarr;</h3>
+            <h3>Curso de Informática Empresarial </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>Com 6 cursos completos em vídeo aulas, você poderá assistir quando e onde quiser pelo celular ou em qualquer outro dispositivo. Não é necessário ter computador para assistir as aulas.
-            </h7>
+            <p>Com 6 cursos completos em vídeo aulas, você poderá assistir quando e onde quiser pelo celular ou em qualquer outro dispositivo. Não é necessário ter computador para assistir as aulas.
+            </p>
           </a>
 
           <a target="_blank"
+            rel="noopener"
           href="https://go.hotmart.com/G39347111D" className={styles.card}>
-            <h3>Curso Auxiliar Administrativo &rarr;</h3>
+            <h3>Curso Auxiliar Administrativo </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>Auxiliar Administrativo - O curso nº 1 para quem quer entrar no mercado de trabalho!</h7>
+            <p>Auxiliar Administrativo - O curso nº 1 para quem quer entrar no mercado de trabalho!</p>
           </a>
 
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/R39651724C"
             className={styles.card}
           >
-            <h3>Como fazer uma Monografia de sucesso &rarr;</h3>
+            <h3>Como fazer uma Monografia de sucesso </h3>
             <h6>Formato: eBooks, Documentos</h6>
-            <h7>Se você está iniciando sua MONOGRAFIA e não sabe por onde começar, este e-book é pra você!</h7>
+            <p>Se você está iniciando sua MONOGRAFIA e não sabe por onde começar, este e-book é pra você!</p>
           </a>
 
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/L47386117Q"
             className={styles.card}
           >
-            <h3>A de Abelha &rarr;</h3>
+            <h3>A de Abelha </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>
+            <p>
             Mais de 700 atividades infantis, para crianças em período de alfabetização, entre 02 a 08 anos.
-            </h7>
+            </p>
           </a>
 
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/Q39355872J"
             className={styles.card}
           >
-            <h3>Site para Negócios &rarr;</h3>
+            <h3>Site para Negócios </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>Aprenda criar Sites Profissionais voltados para Empreendedores que desejam posicionar sua marca na internet e construir autoridade no seu nicho de forma eficiente.</h7>
+            <p>Aprenda criar Sites Profissionais voltados para Empreendedores que desejam posicionar sua marca na internet e construir autoridade no seu nicho de forma eficiente.</p>
           </a>
 
           <a target="_blank"
+            rel="noopener"
           href="https://go.hotmart.com/R41416796C" className={styles.card}>
-            <h3>Curso Completo Manutenção e Conserto de Celular Versão 4.0 &rarr;</h3>
+            <h3>Curso Completo Manutenção e Conserto de Celular Versão 4.0 </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>Curso completo do nível básico até o mais avançado onde o aluno aprende passo-a-passo a manutenção, conserto e reparo em diversos modelos de aparelho de celular de diversos fabricantes.</h7>
+            <p>Curso completo do nível básico até o mais avançado onde o aluno aprende passo-a-passo a manutenção, conserto e reparo em diversos modelos de aparelho de celular de diversos fabricantes.</p>
           </a>
           
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/G29201386F"
             className={styles.card}
           >
-            <h3>Pacote Apostilas Bolos de Sucesso &rarr;</h3>
+            <h3>Pacote Apostilas Bolos de Sucesso </h3>
             <h6>Formato: eBooks, Documentos</h6>
-            <h7>São 11 Apostilas, totalizando mais de 400 receitas completas de massas, coberturas, recheios, brigadeiros gourmet, bolos, tortas e muito mais.
-            </h7>
+            <p>São 11 Apostilas, totalizando mais de 400 receitas completas de massas, coberturas, recheios, brigadeiros gourmet, bolos, tortas e muito mais.
+            </p>
           </a>
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/V39651790O"
             className={styles.card}
           >
-            <h3>Homem de Respeito &rarr;</h3>
+            <h3>Homem de Respeito </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>O Melhor Curso de Sedução da Internet! Torne-se um Homem de Respeito e Aprenda Como Conquistar uma Mulher. Dicas Exclusivas para ser um homem que as mulheres querem, O Curso homem veio para Revolucionar.
-            </h7>
+            <p>O Melhor Curso de Sedução da Internet! Torne-se um Homem de Respeito e Aprenda Como Conquistar uma Mulher. Dicas Exclusivas para ser um homem que as mulheres querem, O Curso homem veio para Revolucionar.
+            </p>
           </a>
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/D29196891G"
             className={styles.card}
           >
-            <h3>Fechamento Inteligente &rarr;</h3>
+            <h3>Fechamento Inteligente </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>O Fechamento Inteligente é um sistema que permite ao apostador otimizar suas apostas de maneira bastante inteligente.
-            </h7>
+            <p>O Fechamento Inteligente é um sistema que permite ao apostador otimizar suas apostas de maneira bastante inteligente.
+            </p>
           </a>
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/V39355871I"
             className={styles.card}
           >
-            <h3>E-Book Instagram para Negócios &rarr;</h3>
+            <h2>Por até R$100,00</h2>
+            <h3>E-Book Instagram para Negócios </h3>
             <h6>Formato: eBooks, Documentos</h6>
-            <h7>Ebook em PDF com dicas essenciais para quem deseja transformar o seu instagram em uma conta comercial, atraente e totalmente profissional, para conquistar mais seguidores, enjangamento e fortalecimento da marca da empresa de forma eficiente e estratégica.
-            </h7>
-            <p>Por até R$100,00</p>
+            <p>Ebook em PDF com dicas essenciais para quem deseja transformar o seu instagram em uma conta comercial, atraente e totalmente profissional, para conquistar mais seguidores, enjangamento e fortalecimento da marca da empresa de forma eficiente e estratégica.
+            </p>
+            
           </a>
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/G39347449T"
             className={styles.card}
           >
-            <h3>Curso Operador de Caixa em Apostila &rarr;</h3>
+            <h3>Curso Operador de Caixa em Apostila </h3>
             <h6>Formato: eBooks, Documentos</h6>
-            <h7>Operador de caixa é uma das áreas que mais crescem e necessitam de pessoas. Faça nosso curso e receba um certificado reconhecido em todo o território nacional.
-            </h7>
+            <p>Operador de caixa é uma das áreas que mais crescem e necessitam de pessoas. Faça nosso curso e receba um certificado reconhecido em todo o território nacional.
+            </p>
           </a>
           <a target="_blank"
+            rel="noopener"
             href="https://go.hotmart.com/K39651548V"
             className={styles.card}
           >
-            <h3>Curso Maquiagem na Web 1.0 &rarr;</h3>
+            <h3>Curso Maquiagem na Web 1.0 </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>O Curso Maquiagem na Web é totalmente Online, disponível por 1 ano, 24 horas por dia. São mais de 35 vídeo aulas+ atualizações de novas aulas + certificado Você pode fazer de onde quiser e em qualquer horário.
-            </h7>
+            <p>O Curso Maquiagem na Web é totalmente Online, disponível por 1 ano, 24 horas por dia. São mais de 35 vídeo aulas+ atualizações de novas aulas + certificado Você pode fazer de onde quiser e em qualquer horário.
+            </p>
           </a>
 
                     
           {/* <a target="_blank"
+            rel="noopener"
             href="#"
             className={styles.card}
           >
-            <h3> &rarr;</h3>
+            <h3> </h3>
             <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
-            <h7>
-            </h7>
+            <p>
+            </p>
           </a> */}
 
 
@@ -171,7 +192,7 @@ export default function Home() {
       <footer className={styles.footer}>
         <a
           href="/"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
           Powered by{' Álef Coutinho'}
           <img src="/logoqualifiquese.png" alt="QualifiqueseOnline Logo" className={styles.logo} />
