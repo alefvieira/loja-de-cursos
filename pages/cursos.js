8405220ca41cf46fd5326cd780eeb2460ddb2e87
alefvieira <78700687+alefvieira@users.noreply.github.com>
2021-02-10 01:37:11 -0300

    Add files via upload
    segunda atualização do projeto

---
+++ pages/cursos.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Alert,
+    Button
+
+} from 'reactstrap';
+
+const AlertExample = (props) => {
+  const [visible, setVisible] = useState(true);
+
+  const onDismiss = () => setVisible(false);
+
+  return (
+    <Alert color="info" isOpen={visible} toggle={onDismiss}>
+      I am an alert and I can be dismissed!
+    </Alert>
+    
+  );
+
+}
+
+export default AlertExample;
+
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,181 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
+
+
   return (
     <div className={styles.container}>
+      
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+      <title>Cursos online - aprenda o que quiser, quando quiser</title>
+        <link rel="icon" href="/logoQO.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    
+        <h3 className={styles.title}>
+          Welcome to <a href="/">NowFormOnline</a>
+        </h3>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+        <a target="_blank"
+            href="https://go.hotmart.com/Y29172716T"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Curso de Desenho Método Fanart 3.0 &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>O Curso de Desenho Método Fan Art 3.0 irá te ensinar a desenhar o seu personagem favorito de Anime/Mangá, mesmo que você tenha o famoso "dom" para desenhar.
+            </h7>
+            
+            <p>Por até R$100,00</p>
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          <a target="_blank"
+            href="https://go.hotmart.com/R39347453G"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Curso Design Gráfico &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>Curso online COMPLETO do iniciante ao profissional. Aprenda a criar artes profissionais e trabalhe para gráficas e agências.
+            </h7>
           </a>
+
+          <a target="_blank"
+            href="https://go.hotmart.com/T39347454X"
+            className={styles.card}
+          >
+            <h3>Curso de Informática Empresarial &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>Com 6 cursos completos em vídeo aulas, você poderá assistir quando e onde quiser pelo celular ou em qualquer outro dispositivo. Não é necessário ter computador para assistir as aulas.
+            </h7>
+          </a>
+
+          <a target="_blank"
+          href="https://go.hotmart.com/G39347111D" className={styles.card}>
+            <h3>Curso Auxiliar Administrativo &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>Auxiliar Administrativo - O curso nº 1 para quem quer entrar no mercado de trabalho!</h7>
+          </a>
+
+          <a target="_blank"
+            href="https://go.hotmart.com/R39651724C"
+            className={styles.card}
+          >
+            <h3>Como fazer uma Monografia de sucesso &rarr;</h3>
+            <h6>Formato: eBooks, Documentos</h6>
+            <h7>Se você está iniciando sua MONOGRAFIA e não sabe por onde começar, este e-book é pra você!</h7>
+          </a>
+
+          <a target="_blank"
+            href="https://go.hotmart.com/L47386117Q"
+            className={styles.card}
+          >
+            <h3>A de Abelha &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>
+            Mais de 700 atividades infantis, para crianças em período de alfabetização, entre 02 a 08 anos.
+            </h7>
+          </a>
+
+          <a target="_blank"
+            href="https://go.hotmart.com/Q39355872J"
+            className={styles.card}
+          >
+            <h3>Site para Negócios &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>Aprenda criar Sites Profissionais voltados para Empreendedores que desejam posicionar sua marca na internet e construir autoridade no seu nicho de forma eficiente.</h7>
+          </a>
+
+          <a target="_blank"
+          href="https://go.hotmart.com/R41416796C" className={styles.card}>
+            <h3>Curso Completo Manutenção e Conserto de Celular Versão 4.0 &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>Curso completo do nível básico até o mais avançado onde o aluno aprende passo-a-passo a manutenção, conserto e reparo em diversos modelos de aparelho de celular de diversos fabricantes.</h7>
+          </a>
+          
+          <a target="_blank"
+            href="https://go.hotmart.com/G29201386F"
+            className={styles.card}
+          >
+            <h3>Pacote Apostilas Bolos de Sucesso &rarr;</h3>
+            <h6>Formato: eBooks, Documentos</h6>
+            <h7>São 11 Apostilas, totalizando mais de 400 receitas completas de massas, coberturas, recheios, brigadeiros gourmet, bolos, tortas e muito mais.
+            </h7>
+          </a>
+          <a target="_blank"
+            href="https://go.hotmart.com/V39651790O"
+            className={styles.card}
+          >
+            <h3>Homem de Respeito &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>O Melhor Curso de Sedução da Internet! Torne-se um Homem de Respeito e Aprenda Como Conquistar uma Mulher. Dicas Exclusivas para ser um homem que as mulheres querem, O Curso homem veio para Revolucionar.
+            </h7>
+          </a>
+          <a target="_blank"
+            href="https://go.hotmart.com/D29196891G"
+            className={styles.card}
+          >
+            <h3>Fechamento Inteligente &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>O Fechamento Inteligente é um sistema que permite ao apostador otimizar suas apostas de maneira bastante inteligente.
+            </h7>
+          </a>
+          <a target="_blank"
+            href="https://go.hotmart.com/V39355871I"
+            className={styles.card}
+          >
+            <h3>E-Book Instagram para Negócios &rarr;</h3>
+            <h6>Formato: eBooks, Documentos</h6>
+            <h7>Ebook em PDF com dicas essenciais para quem deseja transformar o seu instagram em uma conta comercial, atraente e totalmente profissional, para conquistar mais seguidores, enjangamento e fortalecimento da marca da empresa de forma eficiente e estratégica.
+            </h7>
+            <p>Por até R$100,00</p>
+          </a>
+          <a target="_blank"
+            href="https://go.hotmart.com/G39347449T"
+            className={styles.card}
+          >
+            <h3>Curso Operador de Caixa em Apostila &rarr;</h3>
+            <h6>Formato: eBooks, Documentos</h6>
+            <h7>Operador de caixa é uma das áreas que mais crescem e necessitam de pessoas. Faça nosso curso e receba um certificado reconhecido em todo o território nacional.
+            </h7>
+          </a>
+          <a target="_blank"
+            href="https://go.hotmart.com/K39651548V"
+            className={styles.card}
+          >
+            <h3>Curso Maquiagem na Web 1.0 &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>O Curso Maquiagem na Web é totalmente Online, disponível por 1 ano, 24 horas por dia. São mais de 35 vídeo aulas+ atualizações de novas aulas + certificado Você pode fazer de onde quiser e em qualquer horário.
+            </h7>
+          </a>
+
+                    
+          {/* <a target="_blank"
+            href="#"
+            className={styles.card}
+          >
+            <h3> &rarr;</h3>
+            <h6>Formato: Cursos Online, Serviços de Assinatura</h6>
+            <h7>
+            </h7>
+          </a> */}
+
+
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by{' Álef Coutinho'}
+          <img src="/logoqualifiquese.png" alt="QualifiqueseOnline Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
